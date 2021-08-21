a949a93b1749c4a7b5ea9b7e8b28bb5f0c0bf1c3
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-22 01:46:27 +0630

    add core django

---
+++ core/utils.py
@@ -0,0 +1,32 @@
+
+import os
+from pathlib import Path
+import json
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+KEYS_PATH = os.path.join(BASE_DIR,"KEYS.json")
+
+def get_KEY(keyname):
+
+    if os.path.exists(KEYS_PATH):
+        with open(KEYS_PATH) as raw_json:
+            data = json.load(raw_json)
+            return data[keyname]
+    else:
+        return os.environ[keyname]
+
+def is_DEBUG():
+    if os.path.exists(KEYS_PATH):
+        with open(KEYS_PATH) as raw_json:
+            data = json.load(raw_json)
+
+            if data['DEBUG'] == "TRUE":
+                return True
+            else:
+                return False
+
+    else:
+            if os.environ['DEBUG'] == "TRUE":
+                return True
+            else:
+                return False
