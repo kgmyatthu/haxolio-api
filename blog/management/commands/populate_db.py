546de29156fbc7e8b2bc9892a4c5d15c2dffb5fd
Kaung Myat Thu <2962000kaungmyatthu@gmail.com>
2021-08-26 00:31:40 +0630

    database population command run now more efficiently

--- blog/management/commands/populate_db.py
+++ blog/management/commands/populate_db.py
@@ -8,96 +8,80 @@ class Command(BaseCommand):
     
     def handle(self, *args, **kwargs):
         fake = Faker()
-        number_of_entry_to_generate = 200
-        for _ in range(number_of_entry_to_generate):
-            
+        number_of_entry_to_generate = 100
+        tags = []
+        categories = []
+
+        for _ in range (0,8):
+            tags.append(
+                Tag.objects.create(name=fake.word())
+            )
+            categories.append(
+                Category.objects.create(name=fake.word())
+            )
+
+        for i in range(number_of_entry_to_generate):
             article = Article.objects.create(
                     title = fake.paragraph(nb_sentences=2),
                     markdown = readme,
-                    category = Category.objects.create(name=fake.license_plate()),
+                    category = categories[random.randint(0,8-1)],
                     published = True,
                     featured = True,
                 )
 
-            tag1 = Tag.objects.create(name=fake.license_plate())
-            tag2 = Tag.objects.create(name=fake.license_plate())
+            for _ in range(0,random.randint(0,5)):
+                article.tag.add(tags[random.randint(0,8-1)])
 
-            article.tag.add(tag1,tag2)
             article.save()
-            completion = round((_/number_of_entry_to_generate)*100)
+            completion = round((i/number_of_entry_to_generate)*100)
             print(f"{completion}%")
+
+        print(f"100%")
 
 
 readme = """
-# Getting Started with Create React App
+![python version](https://img.shields.io/badge/python-3.8-blue)
+![pip moviepy](https://img.shields.io/badge/pip-moviepy-blue)
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# What is this
+  vdo2gif is a command line tool for converting video file into gif. I find myself opening video editing software just to do simple gif which i'd use to show examples in readme.md of my github repos. This tool is compitable with any python3 compitable system. Tested on windows 10 and linux-debian x86 machines.
+### Features
+ - custom fps, speed, size.
+ 
+# Installation
+  Install python3
+  ```
+  root@kali:~# sudo apt-get install python3
+  ```
+  Install dependencies 
+  ```
+  root@kali:~# sudo python3 -m pip -r requirement.txt
+  ```
+  Install pip if you see something like pip module not found.
+  ```
+    root@kali:~# sudo apt-get install python3-pip
+  ```
+  
+ # Usage
+ ### Converting the whole video file
+ To convert the whole video file to gif
+ ```
+ usage: python3 vdo2gif.py /path/to/input.mp4 /path/to/output.gif 
+ ```
+This example convert the whole video file to gif.
+ 
+ ![image1](https://static.wixstatic.com/media/4cbe8d_f1ed2800a49649848102c68fc5a66e53~mv2.gif)
+ 
+ 
+ ### Converting only a specific part of the video file
+  ```
+ usage: python3 vdo2gif.py /path/to/input.mp4 /path/to/output.gif -t hr:min:sec hr:min:sec 
+ ```
+  This example convert only the part between 5sec and 9sec of the video file
+  
+ ![image1](https://static.wixstatic.com/media/4cbe8d_f1ed2800a49649848102c68fc5a66e53~mv2.gif)
+  
+  # REMINDER TO MYSELF ::
+  -
 
 """
